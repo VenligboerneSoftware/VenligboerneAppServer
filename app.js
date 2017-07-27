cb000b08b845f60064f7a9b1265ee6dee6365376
Mitchell Sayer <mitchell4408@hotmail.com>
2017-07-27 20:51:59 +0200

    Added get request stuff

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
     res.render('index');
 });
+
+app.get('/test', function (req, res) {
+  res.send('AYYY LMAOO')
+})
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
