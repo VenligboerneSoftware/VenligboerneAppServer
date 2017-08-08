96e042fad6982f918b5f63d00a7aec61f09f5bdb
Mitchell Sayer <mitchell4408@hotmail.com>
2017-08-08 22:53:32 +0200

    bugfix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var admin = require("firebase-admin");
+var admin = require('firebase-admin');
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -8,23 +8,23 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-var serviceAccount = require("test-de97f53719d3.json");
+var serviceAccount = require('test-de97f53719d3.json');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://test-b5dbd.firebaseio.com/"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://test-b5dbd.firebaseio.com/'
 });
 var db = admin.database();
 
 app.get('/', function(req, res) {
-    res.render('index');
+	res.render('index');
 });
 
-app.get('/test', async function (req, res) {
-  var ref = db.ref("users");
-  const response = await ref.once("value");
-  res.send("mmm", response.val();
-})
+app.get('/test', async function(req, res) {
+	var ref = db.ref('users');
+	const response = await ref.once('value');
+	res.send('mmm', response.val());
+});
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+	console.log('Our app is running on http://localhost:' + port);
 });
