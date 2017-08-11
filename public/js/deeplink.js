13818eac4b8d37ff158cfe3b8f956af340390eb2
benhannel <ben@bensprojects.com>
2017-08-11 16:55:26 +0200

    Pushing to Heroku

--- public/js/deeplink.js
+++ public/js/deeplink.js
@@ -1,1 +1,15 @@
-window.location.replace(window.location.href.split('?')[1]);
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, '\\$&');
+	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+window.onload = function() {
+	const redirect = getParameterByName('url');
+	console.log('Redirecting to', redirect);
+	window.location.replace(redirect);
+};
