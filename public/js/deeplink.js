b4894660c94b243703cfeeab3f95819ef975572b
benhannel <ben@bensprojects.com>
2017-08-11 13:00:04 +0200

    Pushing to Heroku

--- public/js/deeplink.js
+++ public/js/deeplink.js
@@ -1,28 +1,37 @@
-console.log('hello');
+// console.log('hello');
+//
+// // Attempt to go to the given URL, but if it doesn't exist stay at the current
+// // page
+// function goto(url) {
+// 	// var script = document.createElement('script');
+// 	// script.onload = function() {
+// 	// 	alert('onload');
+// 	// 	document.location = url;
+// 	// };
+// 	// script.onerror = function() {
+// 	// 	alert('onerror');
+// 	// };
+// 	// // use script.onerror to handle bad urls
+// 	// script.setAttribute('src', url);
+// 	// document.getElementsByTagName('head')[0].appendChild(script);
+//
+//   document.location
+// 	// var a = document.createElement('a');
+// 	// a.setAttribute('href', url);
+// 	// a.innerText = url;
+// 	// console.log(document.body);
+// 	// document.body.appendChild(a);
+// 	// a.click();
+// }
+//
+// setTimeout(function() {
+// 	goto(window.location.href.split('?')[1]);
+// }, 100);
 
-// Attempt to go to the given URL, but if it doesn't exist stay at the current
-// page
-function goto(url) {
-	// var script = document.createElement('script');
-	// script.onload = function() {
-	// 	alert('onload');
-	// 	document.location = url;
-	// };
-	// script.onerror = function() {
-	// 	alert('onerror');
-	// };
-	// // use script.onerror to handle bad urls
-	// script.setAttribute('src', url);
-	// document.getElementsByTagName('head')[0].appendChild(script);
-
-	var a = document.createElement('a');
-	a.setAttribute('href', url);
-	a.innerText = url;
-	console.log(document.body);
-	document.body.appendChild(a);
-	a.click();
-}
-
-setTimeout(function() {
-	goto(window.location.href.split('?')[1]);
-}, 100);
+var redirectToApp = function() {
+	setTimeout(function appNotInstalled() {
+		alert('App not installed');
+	}, 100);
+	window.location.replace(window.location.href.split('?')[1]);
+};
+window.onload = redirectToApp;
